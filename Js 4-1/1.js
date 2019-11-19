5b8246edb0b70d99f59c9bb872cd22b2c9cfbbaa
Douglas Henrique <doughrqe@gmail.com>
2019-11-19 17:02:03 -0300

    terceiro exercicio

--- Js 4-1/1.js
+++ Js 4-1/1.js
@@ -1,5 +1,6 @@
 let a = 3,
-    b = 2;
+    b = 2,
+    c = 1;
 console.log(a + b)
 console.log(a - b)
 console.log(a * b)
@@ -9,4 +10,11 @@ if (a > b) {
     console.log(a);
 } else if (b > a) {
     console.log(b);
+}
+if (a > b && a > c) {
+    console.log(a);
+} else if (b > a && b > c) {
+    console.log(b);
+} else if (c > a && c > b) {
+    console.log(c);
 }
