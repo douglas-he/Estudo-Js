af704e0d7b1d7aa1d176c8b35b92e08e815ab841
Douglas Henrique <doughrqe@gmail.com>
2019-11-19 16:58:57 -0300

    Segundo exercicio

--- Js 4-1/1.js
+++ Js 4-1/1.js
@@ -1,7 +1,12 @@
-let a = 1,
+let a = 3,
     b = 2;
 console.log(a + b)
 console.log(a - b)
 console.log(a * b)
 console.log(a / b)
 console.log(a % b)
+if (a > b) {
+    console.log(a);
+} else if (b > a) {
+    console.log(b);
+}
