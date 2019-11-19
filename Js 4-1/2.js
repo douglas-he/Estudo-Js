cf4f5bb0eccd3a8aca9fcdd12aaf917144ae8318
Douglas Henrique <doughrqe@gmail.com>
2019-11-19 18:05:01 -0300

    ex 10

---
+++ Js 4-1/2.js
@@ -0,0 +1,8 @@
+let venda = 1000,
+    valor = 20;
+
+if (valor < 0 || venda < 0) {
+    console.log('valor invalido');
+} else {
+    console.log((venda * valor) - (venda * valor) * (20 / 100));
+}
