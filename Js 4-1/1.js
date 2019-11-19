ba3f5fe3e44e52818446ea13a5056171988d12dd
Douglas Henrique <doughrqe@gmail.com>
2019-11-19 17:34:45 -0300

    Exercicio 6

--- Js 4-1/1.js
+++ Js 4-1/1.js
@@ -24,7 +24,32 @@ if (a > 0) {
     console.log("negative");
 }
 if (a + b + c == 180) {
-    console.log(true)
-} else
-    console.log(false)
+    console.log(true);
+} else {
+    console.log(false);
+}
+let str = "dAma"
+str = str.toLowerCase();
+console.log(str);
+switch (str) {
+    case 'cavalo':
+        console.log('Movimentos em Formato de L')
+        break;
+    case 'bispo':
+        console.log('Movimentos na diagonal')
+        break;
+    case 'peão':
+        console.log('Movimentos apenas para frente, apenas uma casa')
+        break;
+    case 'rei':
+        console.log('Todos os lados, apenas uma casa')
+        break;
+    case 'dama':
+        console.log('todas as direções')
+        break
+    case 'torre':
+        console.log('horizontal e vertical')
+        break;
+    default:
+        console.log('Peça Invalida')
 }
