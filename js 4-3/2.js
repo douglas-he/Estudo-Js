6327e0e4ce7cfd4fd55365b8ede8f763e6cd461d
Douglas Henrique <doughrqe@gmail.com>
2019-11-21 19:21:38 -0300

    Identação

--- js 4-3/2.js
+++ js 4-3/2.js
@@ -2,12 +2,8 @@ x = [150, 123, 100, 115, 44, 88]
 
 function retorna(pipoca) {
     let result = 0;
-    for (let i = 1; pipoca[i] != undefined; i++) {
-
-        if (Math.max(pipoca[result], pipoca[i]) == pipoca[i]) {
-            result = i;
-        }
-    }
+    for (let i = 1; pipoca[i] != undefined; i++)
+        if (Math.max(pipoca[result], pipoca[i]) == pipoca[i]) result = i;
     return result;
 }
-console.log(retorna(x))
+console.log(retorna(x));
