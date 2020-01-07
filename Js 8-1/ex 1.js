9e433032e397cb910566f49d1bec548de050445f
Douglas Henrique <doughrqe@gmail.com>
2020-01-07 17:37:27 -0300

    inicio dia 8-1

---
+++ Js 8-1/ex 1.js
@@ -0,0 +1,13 @@
+// Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of the function. Be certain not to use the var keyword anywhere in your code.
+
+function checkScope() {
+    'use strict';
+    const i = 'function scope';
+    if (true) {
+      const i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+  
