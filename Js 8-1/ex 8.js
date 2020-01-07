cc96aefad90dbec6ce873d3e9e3baf96b4364ac2
Douglas Henrique <doughrqe@gmail.com>
2020-01-07 17:51:57 -0300

    Ex 8

---
+++ Js 8-1/ex 8.js
@@ -0,0 +1,3 @@
+// Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
+const sum = (...args) => args.reduce((a, b) => a + b, 0);
+console.log(sum(1, 2, 3)); // 6
