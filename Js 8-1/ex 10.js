32afc01b8bbbb9dd2dfa1d324b1d744c57c594c3
Douglas Henrique <doughrqe@gmail.com>
2020-01-07 17:58:31 -0300

    Ex 10

---
+++ Js 8-1/ex 10.js
@@ -0,0 +1,17 @@
+// Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables today and tomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // change code below this line
+  
+  const { today, tomorrow } = HIGH_TEMPERATURES;
+  
+  // change code above this line
+  
+  console.log(yesterday) // should be not defined
+  console.log(today); // should be 77
+  console.log(tomorrow); // should be 80
+  
