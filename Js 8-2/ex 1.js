65af34b338c4d4f485b135ad703f6de5bcdc6e60
Douglas Henrique <doughrqe@gmail.com>
2020-01-08 17:46:33 -0300

    Ex 1

--- Js 8-2/ex 1.js
+++ Js 8-2/ex 1.js
@@ -64,7 +64,7 @@ const books = [
 ];
 
 function authorBornIn1947() {
-  
+    return (books.find(element => element.author.birthYear == 1947 ).author.name)
 }
 
 assert.equal(authorBornIn1947(), 'Stephen King');
