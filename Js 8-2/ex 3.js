dfa53e11a64c49cb9c2fe192f60b9998311b0036
Douglas Henrique <doughrqe@gmail.com>
2020-01-08 19:31:44 -0300

    ex 3 não finzalizado

---
+++ Js 8-2/ex 3.js
@@ -0,0 +1,82 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expected_result = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien'
+]
+
+function fantasyOrScienceFictionAuthors() {    
+    // console.log(books.map( Element => Element.author.name));
+    const books2 = (books.filter( Element => Element.genre == 'Fantasia'));
+    const books3= (books.filter( Element => Element.genre == 'Ficção Científica'));
+    console.log(`${(books2.map(Element => Element.author.name))}
+    ${(books3.map(Element => Element.author.name))}`);
+
+}
+fantasyOrScienceFictionAuthors();
+// assert.deepEqual(fantasyOrScienceFictionAuthors(), expected_result);
