79cb1c3101215ffacd99e3a3af4d8bbbdc59aef9
Douglas Henrique <doughrqe@gmail.com>
2020-01-09 02:51:41 -0300

    ex 11

--- Js 8-2/11.js
+++ Js 8-2/11.js
@@ -63,10 +63,11 @@ const books = [
   },
 ];
 
-const expected_result = false
+const expected_result = true
 
-function everyoneWasBornOnSecXX() {
-  return books.every(element => (element.author.birthYear >= 1900 && element.author.birthYear <= 2000))
+function someBookWasReleaseOnThe80s() {
+
+    return books.some(Element=> (Element.releaseYear >= 1980 && Element.releaseYear < 1990 ))
 }
- 
-assert.equal(everyoneWasBornOnSecXX(), expected_result);
+
+assert.equal(someBookWasReleaseOnThe80s(), expected_result);
