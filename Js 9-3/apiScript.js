c64af9c5a4050464736cbaaf938329b8d83256ff
Douglas Henrique <doughrqe@gmail.com>
2020-01-17 18:08:40 -0300

    Exercicio 1

---
+++ Js 9-3/apiScript.js
@@ -0,0 +1,19 @@
+const API_URL = "https://icanhazdadjoke.com/search?term=hipster";
+
+const fetchJoke = () => {
+    fetch(API_URL, {
+        method : 'GET',
+        mode: 'cors',
+        headers: new Headers({
+            Accept : "application/json",
+    })})
+    .then(response => {
+        response.json()
+        .then(response => {
+            document.getElementById('jokeContainer').innerHTML = response.results[0].joke;
+        })
+        .catch(() => console.error('sem Piada'))
+    })
+  .catch(()=> console.error('error'))
+}   
+window.onload = () => fetchJoke()
